caea65a156015386a1c22134fc554947eb75f9b9
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2026-06-01 14:02:01 +0400

    Clarify ML-KEM example flow

--- crypto/mlkem1024/example_test.go
+++ crypto/mlkem1024/example_test.go
@@ -1,31 +1,38 @@
 package mlkem1024_test
 
 import (
+	"bytes"
 	"log"
 
 	"github.com/theQRL/go-qrllib/crypto/mlkem1024"
 )
 
 func Example() {
-	receiverDK, err := mlkem1024.GenerateKey()
+	bobDK, err := mlkem1024.GenerateKey()
 	if err != nil {
 		log.Fatal(err)
 	}
-	receiverEK := receiverDK.EncapsulationKey().Bytes()
 
-	senderEK, err := mlkem1024.NewEncapsulationKey(receiverEK)
-	if err != nil {
-		log.Fatal(err)
-	}
-	senderSharedKey, ciphertext, err := senderEK.Encapsulate()
-	if err != nil {
-		log.Fatal(err)
-	}
-	_ = senderSharedKey
+	// Bob shares his encapsulation key with Alice.
+	bobEKBytes := bobDK.EncapsulationKey().Bytes()
 
-	receiverSharedKey, err := receiverDK.Decapsulate(ciphertext)
+	// Alice encapsulates a shared key to Bob's encapsulation key.
+	bobEK, err := mlkem1024.NewEncapsulationKey(bobEKBytes)
 	if err != nil {
 		log.Fatal(err)
 	}
-	_ = receiverSharedKey
+	aliceSharedKey, ciphertext, err := bobEK.Encapsulate()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Bob decapsulates Alice's ciphertext to recover the same shared key.
+	bobSharedKey, err := bobDK.Decapsulate(ciphertext)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if !bytes.Equal(aliceSharedKey, bobSharedKey) {
+		log.Fatal("shared keys differ")
+	}
 }
